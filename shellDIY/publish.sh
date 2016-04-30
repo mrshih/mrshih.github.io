2d6c80737442b7ebaddc213fdfac7d8a02105337
Andy Shih <daan.shih@gmail.com>
2016-05-01 03:33:45 +0800

    FIX shell git add . 不會增加source裡面文章的Bug

--- shellDIY/publish.sh
+++ shellDIY/publish.sh
@@ -1,5 +1,6 @@
+#這個shell因為git add的關係，必須cd到shellDIY資料夾，才可以RUN
 read -p "Please keyin your new commit message:" mrshihNewCommitMessage
 rake deploy
-git add .
+git add ../source/
 git commit -m "${mrshihNewCommitMessage}"
 git push origin source
