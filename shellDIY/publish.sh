0ae2965940012f13b88b282458c3e0cd7b27f141
Andy Shih <daan.shih@gmail.com>
2016-05-03 22:27:55 +0800

    UPDATE config

--- shellDIY/publish.sh
+++ shellDIY/publish.sh
@@ -1,6 +1,6 @@
 #這個shell因為git add的關係，必須cd到shellDIY資料夾，才可以RUN
 read -p "Please keyin your new commit message:" mrshihNewCommitMessage
 rake deploy
-git add ../source/
+git add ../
 git commit -m "${mrshihNewCommitMessage}"
 git push origin source
